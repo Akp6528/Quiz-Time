e5965d3e5daf1169c39d00536b45f305506f71a9
Ashwani Pandey <pandeyashwani999@gmail.com>
2020-06-29 18:58:03 +0530

    checking

---
+++ precache-manifest.d218b824d73ea753859a393a767a9d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f4b1a3318eefcf782b9db37d8be32f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70014005eeb62e254ea0",
+    "url": "/static/css/main.2c6f53b3.chunk.css"
+  },
+  {
+    "revision": "55d1ea9349af7e18237c",
+    "url": "/static/js/2.19bc350d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19bc350d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70014005eeb62e254ea0",
+    "url": "/static/js/main.e4a084fd.chunk.js"
+  },
+  {
+    "revision": "effdde18cac2fca9e737",
+    "url": "/static/js/runtime-main.c09861b7.js"
+  }
+]);
